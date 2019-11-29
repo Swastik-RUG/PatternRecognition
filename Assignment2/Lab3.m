dafac36b700f13afc7a41276f23c9d3d48377062
S. Satyanarayan Nayak <S4151968@Workspace.rug.nl>
2019-11-29 13:01:29 +0100

    Ädding plot and Mahal dist

--- Assignment2/Lab3.m
+++ Assignment2/Lab3.m
@@ -1,4 +1,4 @@
-meanVec = [3,4];
+meanVec = [3, 4];
 covariance = [1,0;0,2];
 x1 = -10:1:10;
 x2 = -10:1:10;
@@ -8,8 +8,19 @@ rpdf = reshape(pdf, length(y), length(x));
 %surf(x,y,rpdf );
 mesh(x, y, rpdf )
 meanPdf = mean(pdf)';
-testPoint = [10, 10];
-npdf = normpdf(pdf, meanVec, covariance);
-md = mahal(testPoint, pdf);
-%md;
+% Calculating Mahalanobis distance
+meanV = [3;4];
+r10_10 = calcMahal(meanV, covariance, [10;10]);
+fprintf("The Mahalnobis distance for point (10,10) is = %f \n", r10_10);
+r0_0 = calcMahal(meanV, covariance, [0;0]);
+fprintf("The Mahalnobis distance for point (0,0) is = %f \n", r0_0);
+r3_4 = calcMahal(meanV, covariance, [3;4]);
+fprintf("The Mahalnobis distance for point (3,4) is = %f \n", r3_4);
+r6_8 = calcMahal(meanV, covariance, [6;8]);
+fprintf("The Mahalnobis distance for point (6,8) is = %f \n", r6_8);
+
+function md = calcMahal(meanVector, covariance, point)
+    stdPoint = (point - meanVector);
+    md = stdPoint'*inv(covariance)*stdPoint;
+end
 
