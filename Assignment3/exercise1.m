1a0b2d0e6ef783d9bc94ccce998c98cbff34ff05
nivinpkumar <pkumarnivin@gmail.com>
2019-12-06 23:58:15 +0100

    Updated exercise1.m
    Updated exercise1.m

--- Assignment3/exercise1.m
+++ Assignment3/exercise1.m
@@ -7,4 +7,9 @@ std = 2;
 val1 = mean1 -4*std:1:mean1+4*std;
 val2 = mean2 -4*std:1:mean2+4*std;
 
-res = normpdf(val1,mean1,std);
+res1 = normpdf(val1,mean1,std);
+res2 = normpdf(val2,mean2,std);
+figure
+plot(val1,res1,'-',val2,res2,'-.',10,res1,'*')
+area1 = trapz(val1(val1>=10),val1(val1>=10));
+area2 = trapz(val2(val2>=10),val2(val2>=10));
