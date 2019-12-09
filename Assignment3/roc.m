aecbea72bdbdba3f61b6b0be9fbdf45e8a46353b
nivinpkumar <pkumarnivin@gmail.com>
2019-12-09 12:39:56 +0100

    ROC curve and some code in lab1
    ROC curve and some code in lab1

---
+++ Assignment3/roc.m
@@ -0,0 +1,16 @@
+mean1 =5;
+sigma =2;
+mean2 =7;
+
+hitrates =[];
+falsealarms=[];
+for i =0:0.1:10
+    hitrates = [hitrates 1 - normcdf(i,mean2,sigma)];
+    falsealarms = [falsealarms 1 - normcdf(i,mean1,sigma)];
+end
+hitrates(102)=0;
+falsealarms(102)=0;
+plot(falsealarms,hitrates);
+title('ROC');
+ylabel('True Positive rate');
+xlabel('False Positive rate');
