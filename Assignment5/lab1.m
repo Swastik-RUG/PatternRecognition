d8d04b51fb9baaa679e12f7dfe7ed1619ff9e4c2
nivinpkumar <pkumarnivin@gmail.com>
2020-01-07 22:14:06 +0100

    small changes
    small changes

--- Assignment5/lab1.m
+++ Assignment5/lab1.m
@@ -38,6 +38,59 @@ dispgraph(prototypes_arr2,'1-2',training_data,2);
 dispgraph(prototypes_arr3,'2-1',training_data,3);
 dispgraph(prototypes_arr4,'2-2',training_data,4);
 
+mylvq(matA,matB,25,0.01);
+
+function res = mylvq(data1,data2,epochs,learning_rate)
+array1 = zeros(length(data1));
+array2 = ones(length(data2));
+A = [data1 array1];
+B = [data2 array2];
+training_data = [A;B];
+
+rng(1);
+training_data = training_data(randperm(length(training_data)),:);
+
+%setting the epochs and learning rate
+epochs =epochs;
+learning_rate = learning_rate;
+
+%creating prototypes
+prototypes_arr = [A(randi(length(A)), :); A(randi(length(A)),:)];
+prototypes_arr = [prototypes_arr ; B(randi(length(B)), :)];
+%wstar is initialized from the data
+wstar_arr = prototypes_arr(:,1:2);
+wstar_labels = prototypes_arr(:,3);
+error =[];
+
+for i=1:epochs
+    misclassified = 0;
+    for j=1:length(training_data)
+        distance =[];
+        x = training_data(j,1:2);
+        y = training_data(j,3);
+        for k = 1:length(wstar_arr)
+            distance = [distance ; pdist2(x,wstar_arr(k,:), 'squaredeuclidean')];
+        end
+        [m,index] = min(distance);
+        wstar =wstar_arr(index,:);
+        wstar_label = wstar_labels(index);
+        wstar_arr(index,:)=wstar + learning_rate *Psi_function(wstar_label,y) *(x-wstar);
+        if(Psi_function(wstar_label,y) == -1)
+            misclassified = misclassified + 1;
+        end
+    end
+    error = [error; misclassified/length(training_data)];
+end
+%displaying the plot
+figure(4);
+plot(1:epochs,error);
+title('Error Curve');
+xlabel('Epochs');
+ylabel('Error');
+
+end
+
+
 function dispgraph(prototypes_arr, plot_name, training_data, plot_no)
 epochs =25;
 learning_rate = 0.01;
